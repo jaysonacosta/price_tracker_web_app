775bd05e2eb784b1142fbfc9c19f6afe9a95af83
Jayson Acosta <36345453+jaysonacosta@users.noreply.github.com>
2023-01-01 16:33:09 -0500

    created dynamic entry page

---
+++ src/pages/[entryId].tsx
@@ -0,0 +1,19 @@
+import { type NextPage } from "next";
+
+import Head from "next/head";
+import Link from "next/link";
+
+const EntryPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Price Tracker</title>
+        <meta name="description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Entry page</main>
+    </>
+  );
+};
+
+export default EntryPage;
